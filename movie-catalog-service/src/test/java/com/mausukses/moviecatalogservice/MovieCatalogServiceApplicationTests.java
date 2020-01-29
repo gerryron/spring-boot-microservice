fba567b519c39f23be25186d3bc4625fa9fbe9a8
Yhoboo <Gerryron02@gmail.com>
2020-01-29 09:44:55 +0700

    01. init 3 project

---
+++ movie-catalog-service/src/test/java/com/mausukses/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mausukses.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
